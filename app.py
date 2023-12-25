2d2b701d2df7973c4fa2c355100f3fa9d8173892
Victor midega <vbm7752@gmail.com>
2023-12-25 03:37:15 +0300

    New commit

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('home.html')
+def home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
