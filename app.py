5b3cfb6a0127b26d9215dff3f4e667b8a2d361f2
Victor midega <vbm7752@gmail.com>
2023-12-25 19:25:04 +0300

    new files

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 
 
 app = Flask(__name__)
 
+JOBS = [
+
+    {
+    'id' : 1,
+    'title' : 'Data manager',
+    'location' : 'Nairobi, Kenya',
+    'salary' : 'Kes. 25000'
+    },
+
+     {
+    'id' : 1,
+    'title' : 'Data manager',
+    'location' : 'Nairobi, Kenya',
+    'salary' : 'Kes. 25000'
+    },
+
+     {
+    'id' : 1,
+    'title' : 'Data manager',
+    'location' : 'Nairobi, Kenya',
+    
+    },
+
+
+
+]
+
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html",jobs =JOBS )
+
+@app.route('/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
