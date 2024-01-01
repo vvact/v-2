4c2bf0ee2b0f62a9a2fbe0770d17450ed7a13e26
Victor midega <vbm7752@gmail.com>
2024-01-01 05:10:24 +0300

    new

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))      
-    jobs = []
-    for row in result.all():
-        jobs.append(dict(row))
-    return jobs
+    #jobs = []
+    #for row in result.all():
+        #jobs.append(dict(row))
+   # return jobs
     
    
 
