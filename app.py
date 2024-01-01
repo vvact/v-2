0d6e1b462a7824b63e8bddc9a32d393e1e6799ee
Victor midega <vbm7752@gmail.com>
2024-01-01 04:11:14 +0300

    new files

--- app.py
+++ app.py
@@ -1,42 +1,29 @@
 from flask import Flask,render_template,jsonify
-
-
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
-JOBS = [
 
-    {
-    'id' : 1,
-    'title' : 'Data manager',
-    'location' : 'Nairobi, Kenya',
-    'salary' : 'Kes. 25000'
-    },
-
-     {
-    'id' : 1,
-    'title' : 'Data manager',
-    'location' : 'Nairobi, Kenya',
-    'salary' : 'Kes. 25000'
-    },
-
-     {
-    'id' : 1,
-    'title' : 'Data manager',
-    'location' : 'Nairobi, Kenya',
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))      
+    jobs = []
+    for row in result.all():
+        jobs.append(dict(row))
+    return jobs
     
-    },
-]
-
+   
 
 @app.route('/')
 def home():
-    return render_template("home.html",jobs =JOBS,company_name = ' LocalJobs')
+    jobs = load_jobs_from_db()
+    return render_template("home.html",jobs =jobs,company_name = ' LocalJobs')
 
 @app.route('/jobs')
 def list_jobs():
-    return jsonify(JOBS)
+    return jsonify(load_jobs_from_db)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine,text
+
+db_connection_string = "mysql+pymysql://8eb8lwqddbrtop65l8q3:pscale_pw_XaoyNUM70A6XkJLnsdenIaAaecYIo1YRZ0kFieCHE3a@aws.connect.psdb.cloud/victor?charset=utf8mb4"
+
+engine = create_engine(
+db_connection_string,
+connect_args={
+     "ssl": {
+           "ca": "/etc/ssl/cert.pem"
+        }
+}
+)
